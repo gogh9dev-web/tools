06dc6bf8335d74c4fe6c3b13d429c5d83d2794e4
gogh9dev-web <gogh9dev@gmail.com>
2025-11-18 13:58:14 +0900

    Create lang.js

---
+++ assets/lang.js
@@ -0,0 +1,9 @@
+(function() {
+  const lang = navigator.language || navigator.userLanguage;
+
+  if (lang.startsWith("ko")) {
+    window.location.href = "/ko/";
+  } else {
+    window.location.href = "/en/";
+  }
+})();
